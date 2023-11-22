89122c391dcf8b974b4e44570a76c06f5ff4f756
mrezaj79 <45230764+mrezaj79@users.noreply.github.com>
2023-11-22 07:46:54 -0500

    Uncomment transforming to csv files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-rnaseq_tpm  = pd.read_csv("./CCLE_RNAseq_rsem_genes_tpm_20180929.txt", sep='\t')
+rnaseq_tpm  = pd.read_csv("./CCLE_RNAseq_rsem_genes_tpm_20180929.txt.gz",compression="gzip", sep='\t')
 rnaseq_metadata = pd.read_csv("./Cell_lines_annotations_20181226.txt", sep='\t')
 
 MISSING_VALUES_NUMBER = 700
@@ -18,11 +18,7 @@ rnaseq_tpm = rnaseq_tpm.drop(columns=['transcript_ids'])
 # apply Log2(x+0.001) where x is numeric
 rnaseq_tpm = rnaseq_tpm.apply(lambda x: np.log2(x + 0.001) if np.issubdtype(x.dtype, np.number) else x)
 
-print(rnaseq_tpm.shape)
-
-# common_cell_lines = rnaseq_metadata[rnaseq_metadata['CCLE_ID'].isin(rnaseq_tpm['CCLE_ID'])]
-
-
+# Subset rnaseq_metadata based on common cell line names between rnaseq_metadata and rnaseq_tpm 
 common_cell_lines = rnaseq_metadata['CCLE_ID'].isin(rnaseq_tpm.columns)
 rnaseq_metadata = rnaseq_metadata[common_cell_lines]
 
@@ -33,6 +29,6 @@ if not rnaseq_metadata['CCLE_ID'].equals(rnaseq_tpm.columns[1:]):
 
 
 # Save transformed data to CSV files
-# rnaseq_metadata.to_csv("rnaseq_metadata.csv", index=False)
-# rnaseq_tpm.to_csv("rnaseq_tpm.csv", index=False)
+rnaseq_metadata.to_csv("rnaseq_metadata.csv", index=False)
+rnaseq_tpm.to_csv("rnaseq_tpm.csv", index=False)
 
